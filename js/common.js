da0eb7b7ca87eb1da00b9ae7d02623cba4d234f0
Georgiy <goshazvir@gmail.com>
2013-11-30 14:10:21 +0200

    fix

--- js/common.js
+++ js/common.js
@@ -5,5 +5,16 @@ $(document).ready(function() {
 		$(this).parents(".popup").fadeOut("fast");
 	});
 
+	// footer
+	$(function() {
+		var footerHeight = $(".footer").height();
+		$(".out").css("margin-bottom", -footerHeight);
+		$(".push").css("height", footerHeight);
+	});
 
+	$(window).resize(function() {
+		var footerHeight = $(".footer").height();
+		$(".out").css("margin-bottom", -footerHeight);
+		$(".push").css("height", footerHeight);
+	});
 });
